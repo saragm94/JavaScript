e1ab107e3a9e0b7b2ed2345083e32df031e708d2
2DAW10 <2DAW10@DOMDPTOINFJMPP.INF>
2022-10-18 16:20:50 +0200

    Inicio actividad 3

---
+++ 1er Trimestre/DWEC03/script.js
@@ -0,0 +1,9 @@
+function ejer1()
+{
+    var configuracion_ventana = "menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes";
+    window.open("http://www.google.com/", "Pagina Google", configuracion_ventana);
+}
+function ejer2()
+{
+
+}
