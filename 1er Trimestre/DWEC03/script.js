4b668aff7b8c19b255bdc4f78926546334e47ab1
2DAW10 <2DAW10@DOMDPTOINFJMPP.INF>
2022-10-20 19:49:33 +0200

    Adelanto Actividad ventanas

--- 1er Trimestre/DWEC03/script.js
+++ 1er Trimestre/DWEC03/script.js
@@ -1,7 +1,14 @@
 function ejer1()
 {
-    var configuracion_ventana = "menubar=yes,location=yes,resizable=yes,scrollbars=yes,status=yes";
-    window.open("http://www.google.com/", "Pagina Google", configuracion_ventana);
+    var ventana = window.open("ejer1.html", "", "width=800,height=600");
+}
+function ejer1_datos()
+{
+    document.getElementById('datos').innerHTML += "<b>URL completa:</b> " + window.location.href + "</br>";
+    document.getElementById('datos').innerHTML += "<b>Protocolo utilizado:</b> " + window.location.protocol + "</br>";
+    document.getElementById('datos').innerHTML += "<b>Nombre en código del navegador:</b> " +  window.navigator.userAgent + "</br>";
+    document.getElementById('datos').innerHTML += "<b>JAVA disponible:</b> " +  navigator.javaEnabled() + "</br>";
+    document.getElementById('datos').innerHTML += "<iframe src='https://www.google.es'></iframe>" ;
 }
 function ejer2()
 {
